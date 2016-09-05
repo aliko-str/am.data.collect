d42136a0b8db79075ba3add496d31bfe6f6dc8e9
Aliaksei <a.minyukovich@gmail.com>
2016-09-05 15:04:10 +0200

    bug fixes + captcha non-automatic handling

--- data/am.parse.book.page.js
+++ data/am.parse.book.page.js
@@ -1,13 +1,6 @@
 (function run() {
 	const _debug = false;
 	self.port.emit("hello", "HI from the client script. URL: " + window.location.href);
-	// if(!window.AmazonBookPiece2){
-		// console.error("AmazonBookPiece2 isn't defined in the content script");
-		// self.port.emit("error", "AmazonBookPiece2 isn't defined in the content script");
-	// }
-	// self.port.emit("data", doExtraction());
-	
-
 	self.port.on("run", function(){
 		if(!window.AmazonBookPiece2){
 			self.port.emit("error", "AmazonBookPiece2 isn't defined in the content script");
@@ -15,14 +8,22 @@
 		if(_debug){
 			console.log("ABOUT TO RUN page detail Extraction");
 		}
-		self.port.emit("data", doExtraction());
+		var data = doExtraction();
+		if(data == "captcha"){
+			self.port.emit("captcha");
+		}else{
+			self.port.emit("data", data);
+		}
 	});
 })();
-
 
 function doExtraction(){
 	const allBooks = [];
 	const jqRoot = $("div.a-container[role='main']");
+	if(jqRoot.find("h1#title").length < 1){
+		// Amazon is probably asking for Captcha -- let's signal to the addon script
+		return "captcha";
+	}
 
 	const descr = jqRoot.find("iframe#bookDesc_iframe").contents().find("div#iframeContent").text().trim().replace(/\n/g, "");
 	
@@ -43,43 +44,97 @@ function doExtraction(){
 		}
 	}
 	
-	var ifKindle = false, ifPaper = false;
-	const priceRegEx = /\$[\d\.]+/;
-	const onlyCountFormats = ["kindle", "hardcover", "paperback"];
-	const allPrices = jqRoot.find("div#tmmSwatches").find("li").filter(function(){
-		const innerHtml = $(this).text().toLowerCase();
-		for(var aFormat of onlyCountFormats){
-			if(innerHtml.indexOf(aFormat) > -1){
-				if(aFormat == "kindle"){
-					ifKindle = true;
-				}else{
-					ifPaper = true;
-				}
-				return true;
-			}
-		}
-		return false;
-	}).map(function(){
-		const innerText = $(this).find("span.a-button-inner").text();
-		var price = priceRegEx.exec(innerText)[0].replace("$","");
-		price = parseFloat(price);
-		return price;
-	});
-	// if no price is listed - don't count this book
-	if(!allPrices.length){
-		console.log("SKIPPING A BOOK: no price is listed.");
+	// var ifKindle = false, ifPaper = false, avgPrice;
+	const prices = getPrice(jqRoot);
+	if(prices === null){
+		// no price -- skip this book
 		return null;
 	}
-	
-	var priceSum = 0;
-	for(var i = 0; i < allPrices.length; i++){
-		priceSum+= allPrices[i];
-	}
-	const avgPrice = priceSum/allPrices.length;
 	
 	const commentArr = jqRoot.find("div#revMHRL > div").find("div.a-section").map(function(){
 		return $(this).text().trim().replace(/\t/g, "").replace(/\n/g, "");
 	});
 	
-	return new window.AmazonBookPiece2(descr, avgPrice, commentArr, size, ifPaper, ifKindle);
+	return new window.AmazonBookPiece2(descr, commentArr, size, prices.paperPrice, prices.kindlePrice, prices.hardcPrice);
+}
+
+
+function getPrice(jqRoot){
+	var ifMaxPrice = false;
+	var paperPrice, kindlePrice, hardcPrice;
+	
+	const priceRegEx = /\$[\d\.]+/;
+	const onlyCountFormats = ["kindle", "hardcover", "paperback"];
+	function filterOnlyAcceptedFormats(self, mapF){
+		const innerHtml = $(self).text().toLowerCase();
+		for(var aFormat of onlyCountFormats){
+			// I saw cases of not-new book prices -- filter them out
+			if(innerHtml.indexOf(aFormat) > -1 && innerHtml.indexOf("from") == -1){
+				if(aFormat == "kindle"){
+					kindlePrice = mapF(self);
+				}else if(aFormat == "hardcover"){
+					hardcPrice = mapF(self);
+				}else {
+					paperPrice = mapF(self);
+				}
+			}
+		}		
+	}
+	
+	function _mapThePrice1(self){
+		const innerText = $(self).find("span.a-button-inner").text();
+		var price = priceRegEx.exec(innerText);
+		if(price){
+			price = price[0].replace("$","");
+			price = parseFloat(price);
+		}
+		return price;		
+	}
+	
+	function _mapThePrice2(self){
+		// TODO Test if this branch works!!
+		
+		var innerText = $(self).find("span.a-size-base.mediaTab_subtitle").text().trim().split("-");
+		console.log("All prices: ", innerText.join("||"));
+		if(innerText.length > 1){
+			// we will only consider the max price -- min price is for used copies, which is less interesting for us
+			ifMaxPrice = true;
+		}
+		innerText = innerText[innerText.length-1];
+		var price = priceRegEx.exec(innerText);
+		if(price){
+			price = price[price.length-1].replace("$","");
+			price = parseFloat(price);
+		}
+		return price;
+	}
+	
+	function _assignNAifNeeded(a){
+		if(a === null || a === undefined){
+			return "NA";
+		}
+		return a;
+	}
+	
+	jqRoot.find("div#tmmSwatches").find("li span.format").each(function(){
+		filterOnlyAcceptedFormats(this, _mapThePrice1);
+	});
+	
+	// if no price is listed - don't count this book
+	if(paperPrice == undefined && kindlePrice == undefined && hardcPrice == undefined){
+		// there could be another format of an Amazon page -- let's try to extract from it
+		jqRoot.find("ul#mediaTabs_tabSet").find("li").each(function(){
+			filterOnlyAcceptedFormats(this, _mapThePrice2);
+		});
+		
+		if(paperPrice == undefined && kindlePrice == undefined && hardcPrice == undefined){
+			console.log("SKIPPING A BOOK: no price is listed.");
+			return null;			
+		}
+	}
+		
+	paperPrice = _assignNAifNeeded(paperPrice);
+	kindlePrice = _assignNAifNeeded(kindlePrice);
+	hardcPrice = _assignNAifNeeded(hardcPrice);
+	return {paperPrice: paperPrice, kindlePrice: kindlePrice, hardcPrice: hardcPrice};
 }
